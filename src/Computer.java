10108e7223a0aa221376f0cf562c4574c30da096
dener8 <8dener@gmail.com>
2024-05-15 16:46:15 +0900

    feat: 정답 생성하는 Computer 클래스 구현

--- src/Computer.java
+++ src/Computer.java
@@ -1,15 +1,37 @@
 import java.util.Random;
 
 public class Computer {
-    private int answer;
+    private String answer;
+    private Random random;
 
-    public int getAnswer() {
+    public Computer() {
+        answer = generateAnswer();
+        random = new Random();
+    }
+
+    public String getAnswer() {
         return answer;
     }
 
-    public int generateAnswer() {
-//        Random random = new Random();
-//        random.
-        return 1;
+    public String generateAnswer() {
+        return generateRandomNumber();
+    }
+
+    private String generateRandomNumber() {
+        String randomNumber;
+        do {
+            randomNumber = String.valueOf(random.nextInt(900) + 100);
+        } while (!validateNumber(randomNumber));
+
+        return randomNumber;
+    }
+
+    private boolean validateNumber(String number) {
+        if (number.charAt(0) == number.charAt(1)
+            || number.charAt(1) == number.charAt(2)
+            || number.charAt(2) == number.charAt(3)) {
+            return false;
+        }
+        return true;
     }
 }
